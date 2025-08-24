d33d8ed1d959a0b9bf6324e87c22701ce052e9ed
Odair <odavita32@gmail.com>
2025-08-24 02:47:49 -0300

    Entendendo as funções

--- app/src/main/java/com/example/andcompo01/MainActivity.kt
+++ app/src/main/java/com/example/andcompo01/MainActivity.kt
@@ -15,6 +15,11 @@ class MainActivity : ComponentActivity() {
 
             soma(1,2)
             sub(20,2)
+            mult(20,2)
+            div(5,2)
+            divInt(5,2)
+            Quadrado(5,2)
+
 
         }
     }
@@ -23,16 +28,41 @@ class MainActivity : ComponentActivity() {
 fun soma(a:Int, b:Int){
     val som = a + b
     Text(
-        text = "\nA soma é $som !"
+        text = "\nA soma de $a + $b é $som !"
     )
 }
-
-
 @Composable
 fun sub(a:Int, b:Int){
     val som = a - b
     Text(
-        text = "O resultado da Subtração é $som !"
+        text = "\n\nO resultado da Subtração de $a - $b é $som !"
     )
 }
-
+@Composable
+fun mult(a:Int, b:Int){
+    val som = a * b
+    Text(
+        text = "\n\n\nO resultado da Multiplicação de $a x $b é $som !"
+    )
+}
+@Composable
+fun div(a:Int, b:Int){
+    val som = a / b
+    Text(
+        text = "\n\n\n\nO resultado da Divisão de $a / $b é $som !"
+    )
+}
+@Composable
+fun divInt(a:Int, b:Int){
+    val som = a // b
+    Text(
+        text = "\n\n\n\n\nO resultado da Divisão inteira de $a // $b é $som !"
+    )
+}
+@Composable
+fun Quadrado(a:Int, b:Int){
+    val som = a * a
+    Text(
+        text = "\n\n\n\n\n\nO resultado de $a ao quadrado é $som !"
+    )
+}
